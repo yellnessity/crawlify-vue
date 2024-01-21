f5532a1f62c999fa5e3431883f6a882e013a7100
yellnessity <diaz99@mail.ru>
2024-01-21 16:59:44 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        class="mx-auto h-10 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=600"
+        alt="Your Company"
+      />
+      <h2 class="mt-10 text-center text-2xl leading-9 text-gray-400">
+        Crawlify
+      </h2>
+    </div>
+    <div class="text-center mx-auto max-w-2xl">
+      <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        Elevate your Spotify experience
+      </h1>
+      <p class="mt-6 text-lg leading-8 text-gray-600">
+        a web-application that crawls your Spotify streams and unveiling your personal top songs,
+        albums and artists.
+      </p>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <button class="btn-primary w-full">Sign in</button>
+    </div>
+  </main>
+</template>
